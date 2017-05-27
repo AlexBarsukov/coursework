a6ffece7a8cadc74601a7c5955e9bce2c2777eac
Alex Barsukov <alexbarsukov@hotmail.com>
2017-05-27 18:24:45 +0300

    ?>

--- mail.php
+++ mail.php
@@ -9,3 +9,4 @@ $phone = trim($_GET["phone"]);
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 $message = "Имя: $name \nТелефон: $phone;
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+?>
