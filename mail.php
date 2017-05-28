c8deef2b5c4ddce00fcc20608521b3ddc9815b2f
Alex Barsukov <alexbarsukov@hotmail.com>
2017-05-28 13:06:41 +0300

    28 мая
    Добавлен раздел "о нас"
    создан шаблон

--- mail.php
+++ mail.php
@@ -1,14 +1,31 @@
 <?php
-
-// $recepient = "webtechcoursework@1gb.ru";
+// function _mail($from, $to, $name, $phone){
+// $from = "webtechcoursework@1gb.ru";
+// $to = "dreamlifter88@yandex.ru";
 // $sitename = "webtechcoursework.ru";
-
 // $name = trim($_GET["name"]);
 // $phone = trim($_GET["phone"]);
-
 // $pagetitle = "Новая заявка с сайта \"$sitename\"";
 // $message = "Имя: $name \nТелефон: $phone;
-// mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+// mail($from, $to $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $from");
+// }
+
+// function _mail ($from, $to, $message)
+// {	
+// 	$from = "webtechcoursework@1gb.ru";
+// 	$to = "dreamlifter88@yandex.ru";
+// 	$name = trim($_GET["name"]);
+// 	$phone = trim($_GET["phone"]);
+// 	$message =  $name \n $phone;
+// 	mail($to, $message, 
+// 	"From: $from
+// 	Reply-To: $from
+// 	Content-Type: text/plain; charset=utf-8
+// 	Content-Transfer-Encoding: 8bit"
+// );
+// }
+
+// _mail ('webtechcoursework@1gb.ru', 'dreamlifter88@yandex.ru', $message);
 
 function _mail ($from, $to, $name, $phone)
 {
@@ -24,5 +41,10 @@ Content-Transfer-Encoding: 8bit"
 );
 }
 
-_mail ('webtechcoursework@1gb.ru', 'dreamlifter88@yandex.ru', $name, $phone);
+_mail ('webtechcoursework@1gb.ru', 'dreamlifter88@yandex.ru', "Name "+$name, "Phone "+$phone);
+
+
 ?>
+
+
+
